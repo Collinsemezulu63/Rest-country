60dba5dc86a0b81573be509d7a19041443237dd7
Collins Emezulu <collinsemezulu63@gmail.com>
2023-08-29 10:28:55 +0100

    i add filter selection to the rest country api app

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-// Dark mode //
+//!Dark mode //
+///////////////////////
+//////////////////////
+//////////////////////
 document.querySelector(".but2").addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
 
-//Fetch Countries
+//!Fetch Countries//
+////////////////////////
+///////////////////////
+//////////////////////
 document.addEventListener("DOMContentLoaded", () => {
   let Main = document.querySelector("main"),
-    Section = document.querySelector("section"),
-    Input = document.querySelector("input");
+    Section = document.querySelector("section");
   async function fetchCountries() {
     const response = await fetch(`https://restcountries.com/v3.1/all`);
     const data = await response.json();
     console.log(data);
-    // allCountries = data
     data.forEach((country) => {
       let countryCard = document.createElement("div");
       countryCard.classList.add("country-card");
@@ -23,43 +27,26 @@ document.addEventListener("DOMContentLoaded", () => {
           <p><b>Population</b>:${country["population"]}</p>
           <p><b>Region</b>:${country["region"]}</p>
           <p><b>Capital</b>:${country["capital"]}</p>
-          <p><b>Timezone</b>:${country["timezones"]}</p>
+          <p><b>Timezone</b>:${country["timezones"]["0"]}</p>
         </div>
-                    `;
+        `;
       Main.appendChild(countryCard);
     });
   }
   fetchCountries();
-  Input.addEventListener("input", () => {
-    async function fetchSearchCountries() {
-      try {
-        const response2 = await fetch(
-          `https://restcountries.com/v3.1/name/${Input.value}?fullText=true`
-        );
-        const data2 = await response2.json();
-        console.log(data2);
-        if (response2.ok) {
-          Main.style.display = "none";
-        }
-        data2.forEach((country) => {
-          let countryCard = document.createElement("div");
-          countryCard.classList.add("country-card");
-          countryCard.innerHTML = `
-            <img src="${country["flags"].png}" alt="${country["flags"].alt}">
-            <div class="content">
-              <h1>${country["name"].common}</h1>
-              <p><b>Population</b>:${country["population"]}</p>
-              <p><b>Region</b>:${country["region"]}</p>
-              <p><b>Capital</b>:${country["capital"]}</p>
-              <p><b>Timezone</b>:${country["timezones"]}</p>
-            </div>
-                    `;
-          Section.appendChild(countryCard);
-        });
-      } catch (error) {
-        console.log(error);
-      }
+
+  //!Filter selection//
+  /////////////////////////////
+  //////////////////////////////
+  /////////////////////////////
+  document.querySelector("input").addEventListener("input", () => {
+    const input_value = document.querySelector("input").value.toUpperCase(),
+      country_card = document.querySelectorAll(".country-card");
+    for (let index = 0; index < country_card.length; index++) {
+      const Name = country_card[index].querySelector(".content h1").innerText;
+      if (Name.toUpperCase().indexOf(input_value) > -1)
+        country_card[index].style.display = "";
+      else country_card[index].style.display = "none";
     }
-    fetchSearchCountries();
   });
 });
